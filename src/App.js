f37d0a1ed02ed812659a777ab57b42b465fc6ad5
Iylin <kiylin94@naver.com>
2020-12-14 10:19:22 +0900

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import './app.css';
-
 function App() {
   return <h1>Hello :)</h1>;
 }
 
 export default App;
+git 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import App from './App';
+import firebase from './firebase';
+
 
 
 ReactDOM.render(
